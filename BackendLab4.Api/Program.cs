91f5125acef68344ba868d1850337a5eb684a6cd
generalmaks <schrinan@gmail.com>
2025-11-23 19:12:07 +0200

    Added basic configuration for jwt tokens

--- BackendLab4.Api/Program.cs
+++ BackendLab4.Api/Program.cs
@@ -1,9 +1,27 @@
+using System.Text;
 using BackendLab3.Context;
 using BackendLab3.Services.Interfaces;
 using BackendLab3.Services.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddAuthorization();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]!))
+        };
+    });
 
 builder.Services.AddScoped<ICurrencyService, CurrencyService>();
 builder.Services.AddScoped<IUserService, UserService>();
